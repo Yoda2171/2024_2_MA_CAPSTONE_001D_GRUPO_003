91f217c87e270b8f44f73bfed84fbbbbdf9630ca
Richard <rich.emartinez@hotmail.com>
2024-09-15 23:20:17 -0300

    para acordarme de eliminar todo esto

--- Fase 2/Evidencias Proyecto/SalvaPOS/src/app/app.routes.ts
+++ Fase 2/Evidencias Proyecto/SalvaPOS/src/app/app.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },  // Ruta por defecto que carga el HomeComponent
+  { path: '', component: HomeComponent },  // Ruta predeterminada para Home
+  { path: '**', redirectTo: '' },  // Redirige cualquier ruta desconocida a la página de inicio
 ];
